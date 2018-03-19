3897a227d0a58cf83081323188b55a982edda27b
zhangxin <zhangxin@juxinli.com>
2018-03-19 23:31:45 +0800

    用俩个栈实现一个队列，完成队列的Push和Pop操作

---
+++ 3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# 用俩个栈实现一个队列，完成队列的Push和Pop操作。队列中的元素为int类型
+
+class Solution:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, node):
+        self.stack1.append(node)
+
+    def pop(self):
+        if self.stack2 == []:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+            return self.stack2.pop()
+        return self.stack2.pop()
