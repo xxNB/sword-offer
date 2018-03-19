a490b9449eb1f07ff721cc204f21871746b81c8f
zhangxin <zhangxin@juxinli.com>
2018-03-20 00:30:40 +0800

    复杂链表的复制

---
+++ code/3.20_4.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+
+class Solution:
+    # 返回 RandomListNode
+    def Clone(self, pHead):
+        # write code here
+        if pHead is None:
+            return None
+        p = RandomListNode(pHead.label)
+        p.next = pHead.next
+        p.random = pHead.random
+
+        p.next = self.Clone(pHead.next)
+        # 返回复杂链表的head
+        return p
+
+
+添加笔记
