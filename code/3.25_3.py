5049984c9dbe700fec45576dd94406b1d0ad930e
zhangxin <zhangxin@juxinli.com>
2018-03-25 21:04:27 +0800

    数据流中的中位数

---
+++ code/3.25_3.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数
+# 据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
+
+class Solution:
+    def __init__(self):
+        self.data = []
+
+    def Insert(self, num):
+        # write code here
+        self.data.append(num)
+        self.data.sort()
+
+    def GetMedian(self, data):
+        # write code here
+        length = len(self.data)
+        if length % 2 == 0:
+            return (self.data[length // 2] + self.data[length // 2 - 1]) / 2.0
+        else:
+            return self.data[int(length // 2)]
+
+
