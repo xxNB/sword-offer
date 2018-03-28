4573ecb295a3159f87c664fc3d13c15c489be029
zhangxin <zhangxin@juxinli.com>
2018-03-28 21:01:25 +0800

    二叉树的下一个节点

---
+++ code/3.28_1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+# 给定一个二叉树和其中的一个结点，请找出中序遍历(左根右)顺序的下一个结点并且返回。注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+
+
+class Solution:
+    # 根始终是左的下一个节点，出发点在做节点
+    def GetNext(self, pNode):
+        if not pNode:
+            return pNode
+        if pNode.right:
+            left1 = pNode.right
+            while left1.left1:
+                left1 = left1.left
+            return left1
+        while pNode.next:
+            tmp = pNode.next
+            if tmp.left == pNode:
+                return tmp
+            pNode = tmp
