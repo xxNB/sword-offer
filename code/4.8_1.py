502dc4eb68b59300930d4c34332a87a28acd66ae
章昕 <zhangxin@juxinli.com>
2018-04-08 15:11:43 +0800

    归并排序

--- code/4.8_1.py
+++ code/4.8_1.py
@@ -8,6 +8,8 @@ import sys
 def merge(nums, first, middle, last):
     lnums = nums[first:middle + 1]
     rnums = nums[middle + 1:last + 1]
+    print "lnums", lnums
+    print "rnums", rnums
     lnums.append(sys.maxint)
     rnums.append(sys.maxint)
     l = 0
@@ -26,7 +28,9 @@ def merge_sort(nums, first, last):
         middle = (first + last) / 2
         merge_sort(nums, first, middle)
         merge_sort(nums, middle + 1, last)
+        print nums, first, middle, last
         merge(nums, first, middle, last)
+        print "================================"
 
 
 nums = [10, 8, 4, -1, 2, 6, 7, 3]
